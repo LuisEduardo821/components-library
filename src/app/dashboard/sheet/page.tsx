bf23ab8f60f436fe02b986402b752d83e49e266e
Luis Eduardo <lerivadeneira@pucesi.edu.ec>
2024-10-09 23:28:46 -0500

    sheet agregado

---
+++ src/app/dashboard/sheet/page.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { useState } from "react";
+
+export default function SheetPage() {
+  const [open, setOpen] = useState(false);
+  const SHEET_SIDES = ["top", "right", "bottom", "left"] as const;
+  return (
+    <div className="grid grid-cols-4 gap-4">
+      <Sheet open={open} onOpenChange={(open) => setOpen(open)}>
+        <SheetTrigger asChild>
+          <Button>Open</Button>
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+      <Button
+        onClick={() => setOpen(true)}
+        variant="outline"
+        className="col-span-1"
+      >
+        Open Sheet Manual
+      </Button>
+      {SHEET_SIDES.map((side) => (
+        <Sheet key={side}>
+          <SheetTrigger asChild>
+            <Button variant="outline">{side}</Button>
+          </SheetTrigger>
+          <SheetContent side={side}>
+            <SheetHeader>
+              <SheetTitle>Are you absolutely sure?</SheetTitle>
+              <SheetDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      ))}
+    </div>
+  );
+}
