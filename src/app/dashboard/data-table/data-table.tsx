6f68546ea6a963eb2c3ef4467350028a4ff100e1
Luis Eduardo <lerivadeneira@pucesi.edu.ec>
2024-10-19 17:19:33 -0500

    paginacion  agregada

--- src/app/dashboard/data-table/data-table.tsx
+++ src/app/dashboard/data-table/data-table.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import {
   Table,
   TableBody,
@@ -11,9 +12,13 @@ import {
 import {
   ColumnDef,
   flexRender,
+  SortingState,
   getCoreRowModel,
+  getSortedRowModel,
   useReactTable,
+  getPaginationRowModel,
 } from "@tanstack/react-table";
+import { useState } from "react";
 
 interface dataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -23,10 +28,17 @@ export function DataTable<TData, TValue>({
   columns,
   data,
 }: dataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = useState<SortingState>([]);
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+    },
   });
 
   return (
@@ -73,6 +85,24 @@ export function DataTable<TData, TValue>({
           )}
         </TableBody>
       </Table>
+      <div className="flex items-center justify-end space-x-2 py-4 mx-4">
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Previous
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Next
+        </Button>
+      </div>
     </div>
   );
 }
